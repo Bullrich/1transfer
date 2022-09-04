dd7304bff93abe43c59acb6d509e8a63829a233b
Javier Bullrich <javier@bullrich.dev>
2022-09-04 12:16:51 +0200

    feat(client): installed tailwindcss & daisyUI

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
-import sveltePreprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess'
 
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 }
