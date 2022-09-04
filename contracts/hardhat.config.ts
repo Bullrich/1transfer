3e44cd06fb4bb4c4fbbf5c4618f07269de07434d
Javier Bullrich <javier@bullrich.dev>
2022-09-04 13:29:11 +0200

    refactor(hardhat): added outDir to client project
    Now the contract will be built in the client directory

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,6 +3,10 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  typechain: {
+    outDir: "../src/lib/contracts",
+    target: "ethers-v5"
+  }
 };
 
 export default config;
