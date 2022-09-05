62974f1a2e31092a843a70313dd703d61e45d373
Javier Bullrich <javier@bullrich.dev>
2022-09-06 01:16:07 +0200

    feat(navbar): added login btn

---
+++ src/lib/components/LogInButton.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import {signer} from "../stores/crypto";
+</script>
+
+<button class="btn btn-primary" on:click={signer.signIn}>Sign In</button>
--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { APP_NAME } from "../stores";
     import ChainSelect from "./ChainSelect.svelte";
+    import { signer } from "../stores/crypto";
+    import LogInButton from "./LogInButton.svelte";
 </script>
 
 <div class="navbar bg-base-100">
@@ -8,6 +10,10 @@
         <a class="btn btn-ghost normal-case text-xl" href="/">{APP_NAME}</a>
     </div>
     <div class="flex-none gap-2">
-        <ChainSelect />
+        {#if $signer}
+            <ChainSelect />
+        {:else}
+            <LogInButton />
+        {/if}
     </div>
 </div>
