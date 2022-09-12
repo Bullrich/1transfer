ac1bdfb7364fc20201c053ab6c6a822e9eec5a46
Javier Bullrich <javier@bullrich.dev>
2022-09-12 02:05:19 +0200

    feat(client): reload page on network change
    this will ease issues with the blockchain changing and having to update several components.
    
    Now we simply refresh the page and forget about state update

--- src/lib/stores/chain.ts
+++ src/lib/stores/chain.ts
@@ -6,10 +6,7 @@ import type { Chains } from "../utils/chains/chain";
 function createChainStore() {
     const { subscribe, set } = writable<Chains>(parseInt(window.ethereum?.networkVersion));
 
-    window.ethereum?.on("chainChanged", (chainId) => {
-        const idNumber = parseInt(chainId);
-        set(idNumber);
-    });
+    window.ethereum?.on("chainChanged", window.location.reload);
 
     return {
         subscribe,
