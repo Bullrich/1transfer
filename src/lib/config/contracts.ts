9a056bfb6c80e9bc41e4ab38666a678d42fced61
Javier Bullrich <javier@bullrich.dev>
2022-09-15 14:01:35 +0200

    chore(contracts): redeployed contracts

--- src/lib/config/contracts.ts
+++ src/lib/config/contracts.ts
@@ -1,11 +1,17 @@
 import { Chains } from "../utils/chains/chain";
 
-const OPTIMISM_KOVAN = "0xA40267BED8Bbe77a65eD1D318F8F0C32E7eB2B66";
+const OPTIMISM_KOVAN = "0xfd82641C1B3355d283cd03C8ef1DcD87ACBeFBA2";
+const OPTIMISM_GOERLI = "0xe7ac80f3E0630757a580CE10EC3a9BbF2727B882";
+const RINKEBY = "0x0DcC81857eabbeB9d9e6769b36eA9e7Cb5704487";
 
 export function getContractAddress(chain: Chains): string {
     switch (chain) {
         case Chains.OPTIMISM_KOVAN:
             return OPTIMISM_KOVAN;
+        case Chains.OPTIMISM_GOERLI:
+            return OPTIMISM_GOERLI;
+        case Chains.RINKEBY:
+            return RINKEBY;
         default:
             return null;
     }
