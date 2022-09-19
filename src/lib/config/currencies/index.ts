9744857c850b4b3df03aeb9bac85e3825a6eabcf
Javier Bullrich <javier@bullrich.dev>
2022-09-19 18:05:45 +0200

    feat(tokens): added usdc token

--- src/lib/config/currencies/index.ts
+++ src/lib/config/currencies/index.ts
@@ -1,9 +1,12 @@
 import { Chains } from "../../utils/chains";
+import { mainnetCurrencies } from "./mainnet";
 import { optimismGoerliCurrencies } from "./optimismGoerli";
 import type { CurrencyMetadata } from "./types";
 
 export function getTokensFromNetwork(chain: Chains): CurrencyMetadata[] {
     switch (chain) {
+        case Chains.MAINNET:
+            return [mainnetCurrencies];
         case Chains.OPTIMISM_GOERLI:
             return [optimismGoerliCurrencies];
         default:
---
+++ src/lib/config/currencies/mainnet.ts
@@ -0,0 +1,10 @@
+import type { CurrencyMetadata } from "./types"
+import logo from "../../../assets/tokens/tether.webp";
+
+export const mainnetCurrencies: CurrencyMetadata = {
+    symbol: "usdt",
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    image: logo,
+    name: "Tether",
+    decimals: 6
+}
