a63077479a741a59bfde1eb725e17a98a73b1384
Javier Bullrich <javier@bullrich.dev>
2022-09-19 12:41:12 +0200

    feat(store/balance): added handling of currencies

--- src/lib/stores/balance.ts
+++ src/lib/stores/balance.ts
@@ -1,11 +1,22 @@
 import { utils, type BigNumber } from "ethers";
 import { derived, type Readable } from "svelte/store";
+import { ERC20__factory } from "../contracts";
 import { chain } from "./chain";
 import { signer } from "./crypto";
+import { currency } from "./form"
 
-export const balance: Readable<BigNumber> = derived([signer, chain], ([signer, chain], set) => {
+export const balance: Readable<BigNumber> = derived([signer, chain, currency], ([signer, chain, currency], set) => {
     if (signer && chain) {
-        signer.getBalance().then(set);
+        // if not token address is assigned use the default balance
+        if (!currency?.address) {
+            signer.getBalance().then(set);
+        }
+        else if (currency?.address) {
+            // if there is a token address let's get the address of the wallet and then the balance for that token
+            signer.getAddress().then(address => {
+                ERC20__factory.connect(currency.address, signer).balanceOf(address).then(set);
+            });
+        }
     }
 });
 
