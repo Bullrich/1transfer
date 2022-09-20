f73fb3d3463664a445637fc6c2176cc51e32b162
Javier Bullrich <javier@bullrich.dev>
2022-09-20 22:05:56 +0200

    feat(client): added mainnet as default chain
    In the case that the user is not logged, mainnet will be set as default address

--- src/lib/stores/chain.ts
+++ src/lib/stores/chain.ts
@@ -1,11 +1,11 @@
 import { derived, type Readable } from "svelte/store";
 import { getContractAddress } from "../config/contracts";
 import { changeNetwork } from "../utils/chain";
-import type { Chains } from "../utils/chains";
+import { Chains } from "../utils/chains";
 import { signer } from "./crypto";
 
 function createChainStore() {
-    const { subscribe }: Readable<Chains> = derived(signer, () => parseInt(window.ethereum?.networkVersion));
+    const { subscribe }: Readable<Chains> = derived(signer, () => parseInt(window.ethereum?.networkVersion) ?? Chains.MAINNET);
 
     window.ethereum?.on("chainChanged", () => window.location.reload());
 
