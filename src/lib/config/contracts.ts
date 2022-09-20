7cf5d7342720689d16b7b42faadd7e42dcfe9c40
Javier Bullrich <javier@bullrich.dev>
2022-09-20 20:44:42 +0200

    feat(client): updated contract links

--- src/lib/config/contracts.ts
+++ src/lib/config/contracts.ts
@@ -1,8 +1,9 @@
 import { Chains } from "../utils/chains";
 
 const OPTIMISM_KOVAN = "0xfd82641C1B3355d283cd03C8ef1DcD87ACBeFBA2";
-const OPTIMISM_GOERLI = "0xe7ac80f3E0630757a580CE10EC3a9BbF2727B882";
+const OPTIMISM_GOERLI = "0x9ab42Bb61B0b161196f4300b567fAb44ee035e72";
 const RINKEBY = "0x0DcC81857eabbeB9d9e6769b36eA9e7Cb5704487";
+const GOERLI = "0x4a7789c3320d5fE208f65a6aF14Da3FF5b7A6fbf"
 
 export function getContractAddress(chain: Chains): string {
     switch (chain) {
@@ -12,6 +13,8 @@ export function getContractAddress(chain: Chains): string {
             return OPTIMISM_GOERLI;
         case Chains.RINKEBY:
             return RINKEBY;
+        case Chains.GOERLI:
+            return GOERLI;
         default:
             return null;
     }
