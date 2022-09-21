a5922fac2c6c1738d4e4cdb374ad7678df6ef6d6
Javier Bullrich <javier@bullrich.dev>
2022-09-21 19:31:52 +0200

    feat(client): added logo to navbar and favicon

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,11 +3,14 @@
     import ChainSelect from "./ChainSelect.svelte";
     import { signer } from "../stores/crypto";
     import LogInButton from "./LogInButton.svelte";
+    import Logo from "../../assets/logo.svg";
 </script>
 
 <div class="navbar bg-base-100">
     <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl" href="/">{APP_NAME}</a>
+        <a class="btn btn-ghost normal-case text-xl" href="/">
+            <img class="logo" src={Logo} alt="logo" />{APP_NAME}
+        </a>
     </div>
     <div class="flex-none gap-2">
         {#if $signer}
@@ -17,3 +20,9 @@
         {/if}
     </div>
 </div>
+
+<style>
+    .logo {
+        max-height: 70%;
+    }
+</style>
