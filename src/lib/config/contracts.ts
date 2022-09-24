bcf491a0dde6e82a4499258faf63d570e8056522
Javier Bullrich <javier@bullrich.dev>
2022-09-24 22:48:57 +0200

    feat(contract): deployed contract to Optimism

--- src/lib/config/contracts.ts
+++ src/lib/config/contracts.ts
@@ -5,6 +5,7 @@ const OPTIMISM_GOERLI = "0x9ab42Bb61B0b161196f4300b567fAb44ee035e72";
 const RINKEBY = "0x0DcC81857eabbeB9d9e6769b36eA9e7Cb5704487";
 const GOERLI = "0xdefacadafc5907c8c6c2babdf9acf964ceb6f972";
 const MAINNET = "0x0eC74463be41B6145980bB3fd3e54eC983738E9a";
+const OPTIMISM = "0xb2ace012e6787e63e97e20dcf6338d2b4a1f41c6";
 
 export function getContractAddress(chain: Chains): string {
     switch (chain) {
@@ -18,6 +19,8 @@ export function getContractAddress(chain: Chains): string {
             return GOERLI;
         case Chains.MAINNET:
             return MAINNET;
+        case Chains.OPTIMISM:
+            return OPTIMISM;
         default:
             return null;
     }
